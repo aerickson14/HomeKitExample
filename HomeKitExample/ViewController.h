5b91b90ab16adc1899ee4d73423af68fc722ce42
Andrew Erickson <aerickson14@gmail.com>
2014-06-04 08:59:22 -0700

    Initial Commit

---
+++ HomeKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeKitExample
+//
+//  Created by Andrew Erickson on 2014-06-04.
+//  Copyright (c) 2014 Andrew Erickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
